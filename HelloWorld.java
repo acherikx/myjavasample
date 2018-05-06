94459e22efa3e62f9dd61fe9734258b4e386abc4
amarisrama <amarnath.3297@gmail.com>
2018-05-06 12:00:22 +0530

    email support

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void main( String[] args ) {
-        System.out.println( "Hello Jenkins World!" );
+        System.out.println( "Hello Jenkins World! + email support" );
         System.exit( 0 ); //success
     }
 }
